addf0f9d333ded0bc3a9ce4a896ccfa74ecb3a10
Trevor Mackin <tismetrm@gmail.com>
2020-01-31 10:55:22 -0800

    Update the second argument in the Configure method to IHostingEnvironment

--- AnimalShelterApi/Startup.cs
+++ AnimalShelterApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using AnimalShelterApi.Models;
 
 namespace AnimalShelterApi
@@ -40,7 +41,7 @@ namespace AnimalShelterApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
