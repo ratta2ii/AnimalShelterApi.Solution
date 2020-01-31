a3f37dd03aff02b93d4d7c0b3bcd4952a0653ac9
Trevor Mackin <tismetrm@gmail.com>
2020-01-31 10:45:39 -0800

    Add a Pet class with decriptive properties of the Pet Add a context model to store DbSets of all Pets Seed the database with the pets that currently belong to the shelter

---
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AnimalShelterApi.Models
+{
+    public class AnimalShelterApiContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+        public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Pet>()
+                .HasData(
+                    new Pet { PetId = 1, Species = "Cat", Name = "Fred", Color = "black", Age = 7, Available = true },
+                    new Pet { PetId = 1, Species = "Cat", Name = "Chubs", Color = "white", Age = 3, Available = true },
+                    new Pet { PetId = 1, Species = "Dog", Name = "Teddy", Color = "black/white", Age = 5, Available = true },
+                    new Pet { PetId = 1, Species = "Cat", Name = "Billy", Color = "tan", Age = 10, Available = true },
+                    new Pet { PetId = 1, Species = "Dog", Name = "Snoopy", Color = "black/white", Age = 1, Available = true },
+                    new Pet { PetId = 1, Species = "Dog", Name = "Bear", Color = "brown", Age = 6, Available = true },
+                    new Pet { PetId = 1, Species = "Dog", Name = "Hulk", Color = "gray", Age = 11, Available = true },
+                    new Pet { PetId = 1, Species = "Cat", Name = "Medusa", Color = "black", Age = 12, Available = true },
+                    new Pet { PetId = 1, Species = "Dog", Name = "Hercules", Color = "black/brown", Age = 7, Available = true },
+                    new Pet { PetId = 1, Species = "Cat", Name = "Pythagorous", Color = "gray", Age = 9, Available = true }
+                );
+        }
+
+    }
+    
+}
---
+++ AnimalShelterApi/Models/Pet.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System;
+
+namespace AnimalShelterApi.Models
+{
+    public class Pet
+    {
+        public int PetId { get; set; }
+        public string Species { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public bool Available { get; set; }
+    }
+
+}
