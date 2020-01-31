535e0ff18ab20acd07f07cb7f851abeb4f16a43c
Trevor Mackin <tismetrm@gmail.com>
2020-01-31 12:01:10 -0800

    Run migration to build database

---
+++ AnimalShelterApi/Migrations/20200131195957_Initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    PetId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Species = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.PetId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pets",
+                columns: new[] { "PetId", "Age", "Color", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, "black", "Fred", "Cat" },
+                    { 2, 3, "white", "Chubs", "Cat" },
+                    { 3, 5, "black/white", "Teddy", "Dog" },
+                    { 4, 10, "tan", "Billy", "Cat" },
+                    { 5, 1, "black/white", "Snoopy", "Dog" },
+                    { 6, 6, "brown", "Bear", "Dog" },
+                    { 7, 11, "gray", "Hulk", "Dog" },
+                    { 8, 12, "black", "Medusa", "Cat" },
+                    { 9, 7, "black/brown", "Hercules", "Dog" },
+                    { 10, 9, "gray", "Pythagorous", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
